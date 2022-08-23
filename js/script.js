9c966efe58cd32d94c6f8d21a9bdc8bd9831a807
Qodir <kadilov1808@gmail.com>
2022-08-23 21:07:49 +0500

    new project on js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 "use sctrict";
 
-const isChecked = true,
-    isClosed = true;
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(isChecked || isClosed);
+let a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько оцените его?", "");
+    
+    personalMovieDB.movie[a] = b;
+    personalMovieDB.movie[c] = d;
+
+    console.log(personalMovieDB);
+
